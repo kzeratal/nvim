9322d70fa5eb16ddc62d6fd50815b1196cf8f785
Jimmy Kao <skzeratal@gmail.com>
2024-11-08 17:04:48 +0800

    feat: add pyright as python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        opts = { ensure_installed = { "lua_ls", "rust_analyzer", "slint_lsp" } },
+        opts = { ensure_installed = { "lua_ls", "pyright", "rust_analyzer", "slint_lsp" } },
         config = function(_, opts)
             require("mason-lspconfig").setup(opts)
         end,
@@ -16,19 +16,20 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local on_attach_cb = function(_, bufnr)
+                local function map(...)
+                    vim.api.nvim_buf_set_keymap(bufnr, ...)
+                end
+                local args = { noremap = true, silent = true }
+                map("n", "K", "<CMD>lua vim.lsp.buf.hover()<CR>", args)
+                map("n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", args)
+                map("n", "gi", "<CMD>lua vim.lsp.buf.implementation()<CR>", args)
+                map("n", "gr", "<CMD>lua vim.lsp.buf.references()<CR>", args)
+                map("n", "rn", "<CMD>lua vim.lsp.buf.rename()<CR>", args)
+                map("i", "<C-h>", "<CMD>lua vim.lsp.buf.signature_help()<CR>", args)
+            end
             lspconfig.lua_ls.setup({
-                on_attach = function(_, bufnr)
-                    local function map(...)
-                        vim.api.nvim_buf_set_keymap(bufnr, ...)
-                    end
-                    local args = { noremap = true, silent = true }
-                    map("n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", args)
-                    map("n", "K", "<CMD>lua vim.lsp.buf.hover()<CR>", args)
-                    map("n", "gr", "<CMD>lua vim.lsp.buf.references()<CR>", args)
-                    map("n", "gi", "<CMD>lua vim.lsp.buf.implementation()<CR>", args)
-                    map("n", "rn", "<CMD>lua vim.lsp.buf.rename()<CR>", args)
-                    map("i", "<C-h>", "<CMD>lua vim.lsp.buf.signature_help()<CR>", args)
-                end,
+                on_attach = on_attach_cb,
                 settings = {
                     Lua = {
                         diagnostics = {
@@ -43,6 +44,10 @@ return {
                         },
                     },
                 },
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                on_attach = on_attach_cb,
                 capabilities = capabilities,
             })
             lspconfig.slint_lsp.setup({
