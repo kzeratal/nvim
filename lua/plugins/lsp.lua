abc0ab02730ef3fd2cfe739b6f7b08f990be5ca7
kzeratal <skzeratal@gmail.com>
2026-06-29 16:31:39 +0800

    fix: migrate lspconfig to vim.lsp.config API for nvim 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,18 +8,21 @@ return {
         dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local on_attach = function(_, bufnr)
-                local opts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
-                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-            end
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local opts = { noremap = true, silent = true, buffer = args.buf }
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+                end,
+            })
 
             local servers = {
                 bashls = {},
@@ -42,10 +45,10 @@ return {
             })
 
             for server_name, server_config in pairs(servers) do
-                require("lspconfig")[server_name].setup(vim.tbl_extend("force", {
-                    on_attach = on_attach,
+                vim.lsp.config(server_name, vim.tbl_extend("force", {
                     capabilities = capabilities,
                 }, server_config))
+                vim.lsp.enable(server_name)
             end
         end,
     },
