9788e79f602f8950ad6910ab3d515e4a54b16893
kzeratal <skzeratal@gmail.com>
2026-07-02 12:21:50 +0800

    feat: add marksman lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,6 +35,7 @@ return {
                         },
                     },
                 },
+                marksman = {},
                 pyright = {},
                 ruff = {},
                 rust_analyzer = {},
