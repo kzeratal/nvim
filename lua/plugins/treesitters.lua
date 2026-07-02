faf7705d6e4e52917b0a0f31d181bb762659013e
kzeratal <skzeratal@gmail.com>
2026-07-02 14:42:32 +0800

    feat: non-lazy treesitter with universal activation
    Disable lazy loading so parsers are always ready; add a FileType
    autocmd using pcall(vim.treesitter.start) to activate treesitter
    for every buffer; also add markdown parsers.

--- lua/plugins/treesitters.lua
+++ lua/plugins/treesitters.lua
@@ -2,19 +2,29 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        lazy = vim.fn.argc(-1) == 0,
+        lazy = false,
         opts = {
-            highlight = { enable = true },
-            indent = { enable = true },
             ensure_installed = {
                 "bash",
                 "c",
                 "cpp",
                 "lua",
+                "markdown",
+                "markdown_inline",
                 "python",
                 "rust",
                 "sql",
             },
         },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "*",
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
+            })
+        end,
     },
 }
