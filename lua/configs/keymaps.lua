1532f813c3d03fd7c6a315b742cc3d6379ec8368
kzeratal <skzeratal@gmail.com>
2026-07-15 10:58:28 +0800

    feat: auto align screen to the middle

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "j", "v:count == 0 ? 'gjzz' : 'jzz'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gkzz' : 'kzz'", { expr = true, silent = true })
