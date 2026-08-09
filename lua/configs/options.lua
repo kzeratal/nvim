993ec313a4f1dcdb03a9f21e9e2c3e77db18f7ae
kzeretal <skzeretal@gmail.com>
2026-08-09 21:15:30 +0800

    feat: support SSH copy to clipboard

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -6,6 +6,20 @@ vim.opt.expandtab = true
 
 vim.opt.clipboard = "unnamedplus"
 
+if os.getenv("SSH_TTY") then
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        },
+        paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+        },
+    }
+end
+
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.undodir"
 
