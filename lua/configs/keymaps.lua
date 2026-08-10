9d31027565dad73e469c7345c728c1ac58424e8c
kzeratal <skzeratal@gmail.com>
2026-08-10 10:42:56 +0800

    feat: set screen to middle when scroll/search

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -5,3 +5,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "j", "v:count == 0 ? 'gjzz' : 'jzz'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gkzz' : 'kzz'", { expr = true, silent = true })
+
+vim.keymap.set("n", "n", "nzzzv", { silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true })
+vim.keymap.set("n", "*", "*zzzv", { silent = true })
+vim.keymap.set("n", "#", "#zzzv", { silent = true })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
